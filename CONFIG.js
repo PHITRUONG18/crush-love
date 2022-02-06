132d8da735ea63683ca92905f30278c7f612ef9d
PHITRUONG18 <99130258+PHITRUONG18@users.noreply.github.com>
2022-02-06 21:59:59 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -6,4 +6,4 @@ const CONFIGDATA = {
   buttonNo: "Không! Mơ đi cưng.",
   titleModar: "Cảm ơn em vì đồng ý",
   descriptionModar:
-    'Anh biết bé sẽ chọn đồng ý làm người yêu anh mà ^^ </br> Cám ơn em đã đồng ý để cho anh được chở che, bảo vệ cũng như mang lại nụ cười cho em.Thương em cô gái trông trẻ :)) </br> "Anh là một sát thủ tệ, nhưng ngắm em anh bắn không trượt phát nào ". </br>  
+    'Anh biết bé sẽ chọn đồng ý làm người yêu anh mà ^^ </br> Cám ơn em đã đồng ý để cho anh được chở che, bảo vệ cũng như mang lại nụ cười cho em. Thương em cô gái trông trẻ :)) </br> "Anh là một sát thủ tệ, nhưng ngắm em anh bắn không trượt phát nào ". </br>  
